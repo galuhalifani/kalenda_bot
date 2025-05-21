58239f851aad7496a558ac8de09527523bc09ae6
Galuh Alifani <galuh.adika@gmail.com>
2025-05-21 23:01:59 +0700

    enhance prompt

--- prompt.py
+++ prompt.py
@@ -150,20 +150,20 @@ def prompt_analyzer(input, today, timezone=None, event_draft=None, latest_conver
     The scope of the duration of your analysis will be based on the user's input, or, if not specified, based on the earliest start time and the latest end time of the events in the list.
     The scope of time or hours of your analysis will be based on the user's input, or, if not specified, based on the default working hours of 8 AM to 7 PM. 
     If there are events that span the entire day, you will ignore them and add them as remarks, unless the user specifically asks to include them in the analysis.
-    If there are events that are marked as transparent or does not block availability in the calendar, you will ignore them and add them as remarks, unless the user specifically asks to include them in the analysis.
+    If there are events with transparent transparency, will not consider them in the slotting but add them under remarks, unless the user specifically asks to include them in the analysis.
 
     You will return the available time slots grouped by date, in bullet point list, in a human-readable format, including the start and end times of each slot, for example:
 
     *Mon, 19 May 2025:*
     - 10:00 AM - 12:00 PM
     - 2:00 PM - 4:00 PM
-    Please note that the events "A" and "B" are marked as transparent, meaning they do not block your availability. Therefore, I have not considered them as blocking your time.
+    Please note that the events "A" and "B" span the entire day but are marked as transparent, meaning they do not block your availability. Therefore, I have not considered them as blocking your time.
 
     *Tue, 20 May 2025:*
     - 9:00 AM - 11:00 AM
     - 1:00 PM - 3:00 PM
     - 5:00 PM - 7:00 PM
-    Note: You have two events scheduled that span the entire day on this date: Event A, Event B, which is not considered in the above slots. If you consider these events, you do not have any available time slots on this date.
+    Note: You have two events scheduled that span the entire day on this date: Event A, Event B. If you consider these events, you do not have any available time slots on this date.
 
     If user did not specify the time range, you will mention that you are using the default working hours of 8 AM to 7 PM.
 
